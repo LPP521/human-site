b1925ca56904821a1baff633e3590e1a9e0b3ec3
Sawyer <zzuu666@outlook.com>
2017-05-07 13:47:33 +0800

    优化API结构

--- human/views.py
+++ human/views.py
@@ -69,7 +69,6 @@ class AssetList(APIView):
 class AttendanceList(APIView):
     def post(self, request, format=None):
         serializer = AttendanceSerializer(data=request.data)
-        print(request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
